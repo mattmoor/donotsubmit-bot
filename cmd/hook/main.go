ac52af830eae4d806a4b13a5306f4e383ac4baa9
Matt Moore <mattmoor@google.com>
2018-11-21 23:04:26 +0000

    Tweak things to set up for donotsubmit-bot

--- cmd/hook/main.go
+++ cmd/hook/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/mattmoor/hello-github/pkg/github"
+	"github.com/mattmoor/donotsubmit-bot/pkg/github"
 )
 
 func main() {
--- pkg/github/handler.go
+++ pkg/github/handler.go
@@ -11,34 +11,6 @@ import (
 	"github.com/google/go-github/github"
 )
 
-// formatRequest generates ascii representation of a request
-// from: https://medium.com/doing-things-right/pretty-printing-http-requests-in-golang-a918d5aaa000
-func formatRequest(r *http.Request) string {
-	// Create return string
-	var request []string
-	// Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-	// Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
-	// Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	}
-	// Return the request as a string
-	return strings.Join(request, "\n")
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
 	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -55,18 +27,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	event, err := github.ParseWebHook(eventType, payload)
 	if err != nil {
-		log.Printf("ERROR: unable to parse webhook: %v\n%v", err, formatRequest(r))
+		log.Printf("ERROR: unable to parse webhook: %v", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	handleErr := func(event interface{}, err error) {
-		if err == nil {
-			fmt.Fprintf(w, "Handled %T", event)
-			return
-		}
-		log.Printf("Error handling %T: %v", event, err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -74,15 +36,15 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	//   config/one-time/github-source.yaml
 	switch event := event.(type) {
 	case *github.PullRequestEvent:
-		handleErr(event, HandlePullRequest(event))
-	case *github.PushEvent:
-		handleErr(event, HandlePush(event))
-	case *github.PullRequestReviewEvent:
-		handleErr(event, HandleOther(event))
-	case *github.PullRequestReviewCommentEvent:
-		handleErr(event, HandleOther(event))
-	case *github.IssueCommentEvent:
-		handleErr(event, HandleIssueComment(event))
+		if err := HandlePullRequest(event); err != nil {
+			log.Printf("Error handling %T: %v", event, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	case *github.IssuesEvent:
+		if err := HandleIssues(event); err != nil {
+			log.Printf("Error handling %T: %v", event, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	default:
 		log.Printf("Unrecognized event: %T", event)
 		http.Error(w, "Unknown event", http.StatusBadRequest)
@@ -90,53 +52,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func HandleIssueComment(ice *github.IssueCommentEvent) error {
-	log.Printf("Comment from %s on #%d: %q",
-		ice.Sender.GetLogin(),
-		ice.Issue.GetNumber(),
-		ice.Comment.GetBody())
-
-	// TODO(mattmoor): Is ice.Repo.Owner.Login reliable for organizations, or do we
-	// have to parse the FullName?
-	//    Owner: mattmoor, Repo: kontext, Fullname: mattmoor/kontext
-	// log.Printf("Owner: %s, Repo: %s, Fullname: %s", *ice.Repo.Owner.Login, *ice.Repo.Name,
-	// 	*ice.Repo.FullName)
-
-	if strings.Contains(*ice.Comment.Body, "Hello there.") {
-		ctx := context.Background()
-		ghc := GetClient(ctx)
-
-		msg := fmt.Sprintf("Hello @%s", ice.Sender.GetLogin())
-
-		_, _, err := ghc.Issues.CreateComment(ctx,
-			ice.Repo.Owner.GetLogin(), ice.Repo.GetName(), ice.Issue.GetNumber(),
-			&github.IssueComment{
-				Body: &msg,
-			})
-		return err
-	}
-
-	return nil
-}
-
 func HandlePullRequest(pre *github.PullRequestEvent) error {
 	log.Printf("PR: %v", pre.GetPullRequest().String())
-
-	// TODO(mattmoor): To respond to code changes, I think the appropriate set of events are:
-	// 1. opened
-	// 2. reopened
-	// 3. synchronized
-
-	// (from https://developer.github.com/v3/activity/events/types/#pullrequestevent)
-	// Other events we might see include:
-	// * assigned
-	// * unassigned
-	// * review_requested
-	// * review_request_removed
-	// * labeled
-	// * unlabeled
-	// * edited
-	// * closed
 
 	ctx := context.Background()
 	ghc := GetClient(ctx)
@@ -150,12 +67,17 @@ func HandlePullRequest(pre *github.PullRequestEvent) error {
 	return err
 }
 
-func HandlePush(pe *github.PushEvent) error {
-	log.Printf("Push: %v", pe.String())
-	return nil
-}
+func HandleIssues(ie *github.IssuesEvent) error {
+	log.Printf("Issue: %v", ie.GetIssue().String())
 
-func HandleOther(event interface{}) error {
-	log.Printf("TODO %T: %#v", event, event)
-	return nil
+	ctx := context.Background()
+	ghc := GetClient(ctx)
+
+	msg := fmt.Sprintf("Issues event: %v", ie.GetAction())
+	_, _, err := ghc.Issues.CreateComment(ctx,
+		ie.Repo.Owner.GetLogin(), ie.Repo.GetName(), ie.GetIssue().GetNumber(),
+		&github.IssueComment{
+			Body: &msg,
+		})
+	return err
 }
